f4cfda489446f862331ba59a05ea0e41a6ec995d
kurosawaryouta <crogames0304@gmail.com>
2020-04-26 00:51:28 +0900

    360度VR映像の試作のプロジェクトを作った

---
+++ vr360/Assets/VideoTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoTest : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    [SerializeField] Material video_M; //Video再生用マテリアル
+    [SerializeField] Material default_M; //通常シーンのskymaterial
+    [SerializeField] VideoClip videoClip;
+
+    void Start()
+    {
+        var vp = GetComponent<VideoPlayer>();
+        if (vp != null) videoPlayer = vp;
+
+        RenderSettings.skybox = default_M;
+    }
+
+    void Update()
+    {
+        //Pで動画再生
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayVideo(videoClip);
+        }
+    }
+
+    void PlayVideo(VideoClip clip)
+    {
+        //Window>Rendering>Light settingのsky materialを入れ替える
+        RenderSettings.skybox = video_M;
+        videoPlayer.clip = clip;
+    }
+}
